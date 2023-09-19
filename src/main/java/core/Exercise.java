b33f40e2f25ca363d70a58e326ae45c6f41f0fcf
David Salguero Spilde <davidssp@stud.ntnu.no>
2023-09-19 21:39:55 +0200

    chore(Java logic code): :beers: Add Java logic to core folder
    Added java logic to java files located in the core folder
    
    Closes issue 20

--- src/main/java/core/Exercise.java
+++ src/main/java/core/Exercise.java
@@ -1,5 +1,65 @@
 package core;
 
-public class Exercise {
+public class Exercise implements Comparable<Exercise>{
     
+    private String name;
+    private String muscleGroup;
+    private int reps;
+    private int weight;
+
+    public Exercise(String name, String muscleGroup, int reps, int weight) {
+        this.name = name;
+        this.muscleGroup = muscleGroup;
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    public Exercise(String string, String string2) {
+        this.name = string;
+        this.muscleGroup = string2;
+    }
+
+    @Override
+    public int compareTo(Exercise arg0) {
+        //Use Collections.sort(list name) to sort the list based on muscle group
+        return this.muscleGroup.compareTo(arg0.getMuscleGroup());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return name+"," + muscleGroup+",";
+    }
+
 }
--- src/main/java/core/Workout.java
+++ src/main/java/core/Workout.java
@@ -1,5 +1,39 @@
 package core;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Workout {
+    
+    private LocalDate date;
+    private List<Exercise> exercises = new ArrayList<>();
+
+    public Workout(LocalDate date, List<Exercise> exercises) {
+        this.date = date;
+        this.exercises = exercises;
+    }
+
+    @Override
+    public String toString() {
+        return exercises.toString().replaceAll("[]", "");
+    }
+
+    public static List<Exercise> makeList(String line) {
+        String[] items = line.split(",");
+        List<Exercise> list = new ArrayList<>();
+        for (int i = 0; i < items.length; i += 2) {
+            list.add(new Exercise(items[i], items[i + 1]));
+        }
+        return list;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
     
 }
