821c0aecfec839d55c2ce76aff788e1d5f033c5d
Arman Nemati <armanin@stud.ntnu.no>
2023-10-05 23:36:27 +0200

    chore(PlanView): Created PlanGridHandler

--- workoutplanner/fxui/src/main/java/workoutplanner/ui/WorkoutCell.java
+++ workoutplanner/fxui/src/main/java/workoutplanner/ui/WorkoutCell.java
@@ -1,4 +1,15 @@
 package workoutplanner.ui;
+
+import javafx.scene.Group;
+import javafx.scene.layout.HBox;
+import workoutplanner.core.Workout;
 public class WorkoutCell {
-    
+
+    public WorkoutCell(Workout w) {
+
+    }
+
+    public Group getGroup() {
+        return null;
+    }
 }
