cc65dc5bc410ccc26b15bd62ef226bb59c7ae343
Arman Nemati <armanin@stud.ntnu.no>
2023-10-05 23:48:33 +0200

    chore(PlanView): Created WorkoutCell

--- workoutplanner/fxui/src/main/java/workoutplanner/ui/WorkoutCell.java
+++ workoutplanner/fxui/src/main/java/workoutplanner/ui/WorkoutCell.java
@@ -2,14 +2,18 @@ package workoutplanner.ui;
 
 import javafx.scene.Group;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import workoutplanner.core.Workout;
 public class WorkoutCell {
+    private final Workout workout;
 
     public WorkoutCell(Workout w) {
-
+        this.workout = w;
     }
 
     public Group getGroup() {
-        return null;
+        Group group = new Group();
+        group.getChildren().add(new Text(workout.toString()));
+        return group;
     }
 }
