1a10451d55ae42d0c3ce3be9a2579b897f9014ed
David Salguero Spilde <davidssp@stud.ntnu.no>
2023-10-10 13:50:54 +0200

    build: :arrow_up: Added jackson to fxutil
    Added jackson to pom and module under fxutil folder

--- workoutplanner/fxutil/src/main/java/module-info.java
+++ workoutplanner/fxutil/src/main/java/module-info.java
@@ -2,8 +2,8 @@ module fxutil {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
-    // requires transitive workoutplanner.core;
-
+    requires transitive workoutplanner.core;
+    requires com.fasterxml.jackson.databind;
     // exports fxutil.doc;
     // opens fxutil.doc to javafx.graphics, javafx.fxml;
 
