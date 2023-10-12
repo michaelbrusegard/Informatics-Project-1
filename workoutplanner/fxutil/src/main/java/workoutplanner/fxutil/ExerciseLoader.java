8cfee2d7b193f9a476717f8368c0e2d4ff8fb3ca
Arman Nemati <armanin@stud.ntnu.no>
2023-10-12 14:57:22 +0200

    refactor(ExerciseLoader): ExerciseLoader works

--- workoutplanner/fxutil/src/main/java/workoutplanner/fxutil/ExerciseLoader.java
+++ workoutplanner/fxutil/src/main/java/workoutplanner/fxutil/ExerciseLoader.java
@@ -5,24 +5,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.List;
 
 public class ExerciseLoader {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final static ObjectMapper objectMapper = new ObjectMapper();
 
     // Method to load exercises from a JSON file as a URL
     public static List<String> loadExercisesFromJson() throws IOException {
         // Get the URL of the resource
-        URL resourceURL = ExerciseLoader.class.getResource("exercices.json");
+        String resourcePath = "/workoutplanner/fxutil/exercises.json";
+        InputStream resourceURL = ExerciseLoader.class.getResourceAsStream(resourcePath);
 
         if (resourceURL == null) {
-            throw new IOException("Resource not found: exercices.json");
+            throw new IOException("Resource not found: exercises.json");
         }
 
         // Open an InputStream from the URL
-        try (InputStream inputStream = resourceURL.openStream()) {
-            return objectMapper.readValue(inputStream, new TypeReference<List<String>>() {
+        try (InputStream inputStream = resourceURL) {
+            return objectMapper.readValue(inputStream, new TypeReference<>() {
             });
         }
     }
