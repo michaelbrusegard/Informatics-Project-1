80777b4df430c76a07ad59a16957dd4a4f99f827
Michael Brusegard <michbrus@stud.ntnu.no>
2023-10-16 20:32:39 +0200

    feat: renamed checkstyles and create user class

--- workoutplanner/core/src/main/java/workoutplanner/core/User.java
+++ workoutplanner/core/src/main/java/workoutplanner/core/User.java
@@ -1,5 +1,24 @@
 package workoutplanner.core;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+    List<Workout> workouts;
+
+    public User() {
+        this.workouts = new ArrayList<Workout>();
+    }
+
+    public void addWorkout(Workout workout) {
+        this.workouts.add(workout);
+    }
+
+    public void removeWorkout(Workout workout) {
+        this.workouts.remove(workout);
+    }
+
+    public List<Workout> getWorkouts() {
+        return this.workouts;
+    }
 }
