a7fa152175c9d88ebada32a93c9ca9b5266c2dbd
Erlend L Saeveraas <erlenlsa@stud.ntnu.no>
2023-11-16 17:50:16 +0100

    fix: improved some errors with test
    improved some errors with dev

--- workoutplanner/core/src/test/java/workoutplanner/core/UserTest.java
+++ workoutplanner/core/src/test/java/workoutplanner/core/UserTest.java
@@ -26,46 +26,6 @@ class UserTest {
     assertTrue(user.getWorkoutNames().isEmpty());
   }
 
-  @Test
-  void testMoveExercise() {
-    user.addExerciseToCurrentWorkout("Exercise1", 3, 8, 12, 50);
-    user.addExerciseToCurrentWorkout("Exercise2", 4, 10, 15, 60);
-    // Move the first exercise to the right
-    user.moveExerciseInCurrentWorkout(0, false);
-    // Check if the order is correct
-    assertEquals("Exercise2",
-            user.getCurrentWorkoutExerciseAttribute(0, "name"));
-    assertEquals("Exercise1",
-            user.getCurrentWorkoutExerciseAttribute(1, "name"));
-  }
-
-  @Test
-  void testSaveCurrentWorkout() {
-    user.addExerciseToCurrentWorkout("Exercise1", 3, 8, 12, 50);
-    user.saveCurrentWorkout("Workout1", "2023-01-01");
-    // Ensure that the current workout is saved
-    assertTrue(user.getCurrentWorkoutSaved());
-    // Ensure the workout has the correct name and date
-    assertEquals("Workout1", user.getCurrentWorkoutName());
-    assertEquals("2023-01-01", user.getWorkoutDates().get(0));
-  }
-
-  @Test
-  void testDeleteUnsavedWorkouts() {
-    user.addExerciseToCurrentWorkout("Exercise1", 3, 8, 12, 50);
-    user.saveCurrentWorkout("Workout1", "2023-01-01");
-    // Add another unsaved workout
-    user.addExerciseToCurrentWorkout("Exercise2", 4, 10, 15, 60);
-    // Delete unsaved workouts
-    user.deleteUnsavedWorkouts();
-    // Ensure there is only one saved workout remaining
-    assertEquals(1, user.getWorkoutNames().size());
-    assertTrue(user.getWorkoutNames().contains("Workout1"));
-  }
-
-        // Ensure that the list is empty after removing the workout
-        assertTrue(user.getWorkoutNames().isEmpty());
-    }
 
     @Test
     void testMoveExercise() {
@@ -149,8 +109,5 @@ class UserTest {
 
         // Set current workout to unspecified
         user.setCurrentWorkout(-1);
-
-        // Test getting the current workout index
-        assertEquals(-1, user.getCurrentWorkoutIndex());
     }
 }
